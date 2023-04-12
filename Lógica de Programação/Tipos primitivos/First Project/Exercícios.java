3937670bc714a122f50c406f90034373071d40d2
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-04-11 23:02:42 -0300

    bug fix

--- Lógica de Programação/Tipos primitivos/First Project/Exercícios.java
+++ Lógica de Programação/Tipos primitivos/First Project/Exercícios.java
@@ -15,12 +15,13 @@ public class Exercícios {
         System.out.printf("Velocidade média do João: %.2f m/s e %.2f km/h %n", velocidade, velocidade * 3.6);
 
 
-        //Ex 2 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Uma empresa corta barras de aço em pedaços de tamanhos iguais. Construa um programa em que o usuário digita o tamanho da barra de aço e o programa mostra o que sobra da barra de aço depois de cortá-la em pedaços de 5 metros até não ser mais possível.");
-        System.out.print("Digite o comprimento da barra: ");
-        double comprimento = input.nextDouble();
-        System.out.printf("Os pedaços serão de: %.2f %n", comprimento % 5);
+        try (//Ex 2 
+        Scanner input = new Scanner(System.in)) {
+            System.out.println("Uma empresa corta barras de aço em pedaços de tamanhos iguais. Construa um programa em que o usuário digita o tamanho da barra de aço e o programa mostra o que sobra da barra de aço depois de cortá-la em pedaços de 5 metros até não ser mais possível.");
+            System.out.print("Digite o comprimento da barra: ");
+            double comprimento = input.nextDouble();
+            System.out.printf("Os pedaços serão de: %.2f %n", comprimento % 5);
+        }
 
     }
     
