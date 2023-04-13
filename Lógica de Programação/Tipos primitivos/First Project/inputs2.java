2c6381915db4bbe727e7c833b31e03c1ac49b288
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-04-13 00:05:06 -0300

    tipos primitivos

--- Lógica de Programação/Tipos primitivos/First Project/inputs2.java
+++ Lógica de Programação/Tipos primitivos/First Project/inputs2.java
@@ -7,14 +7,20 @@ public class inputs2 {
         String myString;
         double valor;
 
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Entre com um char: ");
+            myChar = input.nextLine().charAt(0);
 
-        System.out.print("Entre com um char: ");
-        myChar = input.nextLine().charAt(0);
+            System.out.print("Entre com uma string: ");
+            myString = input.nextLine();
+            
+            System.out.print("Entre com um double: ");
+            valor = input.nextDouble();
+        }
 
-        
-
-
+        System.out.printf("myChar: %c%n", myChar);
+        System.out.printf("MyString: %s%n", myString);
+        System.out.printf("Valor: %f%n", valor);
     }
 
 }
