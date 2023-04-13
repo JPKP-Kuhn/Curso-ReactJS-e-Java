103eb1d238a1e6331cc0cf6f800b0d1a4b6eae47
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2023-04-12 23:55:24 -0300

    Aula 3

---
+++ Lógica de Programação/Tipos primitivos/First Project/booleano.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class booleano {
+    public static void main(String[] args) {
+            System.out.printf("Result is %b%n", true);
+            System.out.printf("Result is %b%n", !true);
+            System.out.printf("Result is %b%n", 2 > 5);
+            System.out.println("Fazer um programa que execute a multiplicação de dois valores lidos. E se o resultado da multiplicação for menor que 25, mostrar a metade do resultado.");
+
+            Scanner input = new Scanner (System.in);
+            System.out.print("Digite um número: ");
+            double num1 = input.nextDouble();
+
+            System.out.print("Digite outro número: ");
+            double num2 = input.nextDouble();
+
+            double multiplicacao = num1 * num2;
+            
+
+            //Operadores lógicos
+            if (multiplicacao < 25 || multiplicacao > 50){
+                System.out.printf("A multiplicação é %.2f e a sua metade é %.2f %n ", multiplicacao, multiplicacao/2.0);
+            }
+            else{
+                System.out.printf("A multiplicação é %.2f %n", multiplicacao);
+            
+            }
+
+            System.out.print("Digite um número inteiro: ");
+            int n1 = input.nextInt();
+            if (!(n1 < 0)){
+                System.out.println("Número não negativo");
+            }
+            if (n1 > 2 && n1 <= 9){
+                System.out.println("Inteiro no intervalo ]2,9]");
+            }
+            if (n1 > 10 || n1 < 0){
+                System.out.println("Inteiro maior do que 10 ou negativo");
+            }
+            input.close();
+        
+    }
+    
+}
---
+++ Lógica de Programação/Tipos primitivos/First Project/condicional.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class condicional {
+    
+    public static void main(String[] args) {
+        System.out.println("Cálculo de média de nota");
+        try (Scanner input = new Scanner(System.in)){ 
+        
+            System.out.print("Digite a primeira nota: ");
+            double nota1 = input.nextDouble();
+
+            System.out.print("Digite a segunda nota: ");
+            double nota2 = input.nextDouble();
+
+            double media = (nota1 + nota2)/2.0;
+            System.out.printf("Média do aluno: %.2f.", media );
+
+            if (media >= 7){
+                System.out.println("Aluno aprovado!");
+            }
+            else{
+                System.out.println(" Aluno não aprovado");
+            }
+
+        };
+    }
+}
